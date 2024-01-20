1bce5757254bcd5749a269ab3b65ffe6b49c9aa4
salihgueler <muhammedsalihguler@gmail.com>
2024-01-20 15:51:28 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/dev/salih/triv_ai/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.salih.triv_ai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TrivAIApp());
+}
+
+class TrivAIApp extends StatelessWidget {
+  const TrivAIApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
